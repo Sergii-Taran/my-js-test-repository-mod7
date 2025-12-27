e61e2894b36390df9e896b77492ea4854f1bafd7
Sergii Taran <serg.taran1970@gmail.com>
2025-12-27 06:51:02 +0200

    add solution task-07-add-list-item from module 07

--- js/07-add-list-item.js
+++ js/07-add-list-item.js
@@ -1,18 +1,46 @@
+// const titleEl = document.createElement('h1');
+
+// titleEl.classList.add('page-title');
+// titleEl.textContent = 'My title';
+
+// document.body.append(titleEl);
+
+// const imageEl = document.createElement('img');
+
+// imageEl.src =
+//   'https://cdn.pixabay.com/photo/2018/07/26/07/45/valais-3562988_1280.jpg';
+// imageEl.alt = 'valais-alpine-mountains-glacier';
+// imageEl.width = 320;
+
+// document.body.append(imageEl);
+
+// const heroEl = document.querySelector('.hero');
+// heroEl.append(titleEl, imageEl);
+
+// const navItemEl = document.createElement('li');
+// navItemEl.classList.add('site-nav__item');
+
+// const navLinkEl = document.createElement('a');
+// navLinkEl.classList.add('site-nav__link');
+// navLinkEl.textContent = 'Profile';
+// navLinkEl.href = '/profile';
+
+// navItemEl.append(navLinkEl);
+
+// const navEl = document.querySelector('.site-nav');
+// navEl.append(navItemEl);
+
+// ------------------------------ //
+
 const titleEl = document.createElement('h1');
-
-titleEl.classList.add('page-title');
 titleEl.textContent = 'My title';
-
-document.body.append(titleEl);
+titleEl.classList.add('hero-title');
 
 const imageEl = document.createElement('img');
-
 imageEl.src =
   'https://cdn.pixabay.com/photo/2018/07/26/07/45/valais-3562988_1280.jpg';
 imageEl.alt = 'valais-alpine-mountains-glacier';
 imageEl.width = 320;
-
-document.body.append(imageEl);
 
 const heroEl = document.querySelector('.hero');
 heroEl.append(titleEl, imageEl);
@@ -21,9 +49,9 @@ const navItemEl = document.createElement('li');
 navItemEl.classList.add('site-nav__item');
 
 const navLinkEl = document.createElement('a');
-navLinkEl.classList.add('site-nav__link');
-navLinkEl.textContent = 'Profile';
 navLinkEl.href = '/profile';
+navLinkEl.textContent = 'Profile';
+navLinkEl.classList.add('site-nav__link');
 
 navItemEl.append(navLinkEl);
 
