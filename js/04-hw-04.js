07505048529f02295def5072a7b9f1db0b4df0dc
Sergii Taran <serg.taran1970@gmail.com>
2025-12-29 23:12:05 +0200

    add solution homework-04 var.01 from module 07

--- js/04-hw-04.js
+++ js/04-hw-04.js
@@ -1,6 +1,33 @@
+// const formEl = document.querySelector('.login-form');
+
+// formEl.addEventListener('submit', (event) => {
+//   event.preventDefault();
+
+//   const email = formEl.elements.email.value.trim();
+//   const password = formEl.elements.password.value.trim();
+
+//   if (!email || !password) {
+//     alert('All form fields must be filled in');
+//     return;
+//   }
+
+//   const formData = {
+//     email: email,
+//     password: password,
+//   };
+
+//   console.log(formData);
+
+//   formEl.reset();
+// });
+
+// --------------------- //
+
 const formEl = document.querySelector('.login-form');
 
-formEl.addEventListener('submit', (event) => {
+formEl.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
   event.preventDefault();
 
   const email = formEl.elements.email.value.trim();
@@ -19,6 +46,4 @@ formEl.addEventListener('submit', (event) => {
   console.log(formData);
 
   formEl.reset();
-});
-
-// --------------------- //
+}
