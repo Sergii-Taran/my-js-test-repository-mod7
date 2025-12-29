e8c4de4eaa7fcb0e25bf541b9db4e91a24c83d0a
Sergii Taran <serg.taran1970@gmail.com>
2025-12-29 22:46:10 +0200

    add solution homework-03 var.02 from module 07

--- js/03-hw-03.js
+++ js/03-hw-03.js
@@ -12,6 +12,6 @@ const inputEl = document.querySelector('#name-input');
 const outputEl = document.querySelector('#name-output');
 
 inputEl.addEventListener('input', () => {
-  const inputName = inputEl.value.trim();
-  outputEl.textContent = inputName === '' ? 'Anonymous' : inputName;
+  const name = inputEl.value.trim();
+  outputEl.textContent = name || 'Anonymus';
 });
