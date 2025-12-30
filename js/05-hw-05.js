ece2aac773be76a76fe5c16a746df5835102759c
Sergii Taran <serg.taran1970@gmail.com>
2025-12-30 07:47:35 +0200

    add solution homework-05 var.03 from module 07

--- js/05-hw-05.js
+++ js/05-hw-05.js
@@ -17,6 +17,27 @@
 
 // --------------------- //
 
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+// const btnEl = document.querySelector('.change-color');
+// const spanEl = document.querySelector('.color');
+
+// btnEl.addEventListener('click', handleClick);
+
+// function handleClick() {
+//   const randomColor = getRandomHexColor();
+
+//   document.body.style.backgroundColor = randomColor;
+
+//   spanEl.textContent = randomColor;
+// }
+
+// --------------------- //
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
@@ -25,15 +46,14 @@ function getRandomHexColor() {
 
 const btnEl = document.querySelector('.change-color');
 const spanEl = document.querySelector('.color');
+const bodyEl = document.body;
 
 btnEl.addEventListener('click', handleClick);
 
 function handleClick() {
   const randomColor = getRandomHexColor();
 
-  document.body.style.backgroundColor = randomColor;
+  bodyEl.style.backgroundColor = randomColor;
 
   spanEl.textContent = randomColor;
 }
-
-// --------------------- //
